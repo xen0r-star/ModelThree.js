d35f7279b0eb0cf59fec1bae6b3613f41e1add00
Florian <98989004+xen0r-star@users.noreply.github.com>
2023-12-12 14:54:39 +0100

    Add file

---
+++ config/webpack.config.js
@@ -0,0 +1,62 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const path = require('path')
+const paths = require('./paths')
+const fs = require('fs');
+
+
+// Template
+const templates = fs.readdirSync(paths.template);
+const templateContents = templates.map(template => {
+  return fs.readFileSync(path.join(paths.template, template), 'utf8');
+}).join('\n');
+
+
+// Configuration
+module.exports = {
+  entry: [paths.src + '/index.js'],
+
+  output: {
+    path: paths.build,
+    filename: 'js/[name].js',
+    publicPath: '/',
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: paths.src + '/index.html',
+      inject: 'body',
+      content: templateContents,
+      filename: 'index.html',
+      minify: {
+        minifyJS: true,
+        minifyCSS: true,
+        removeComments: true,
+        useShortDoctype: true,
+        collapseWhitespace: true,
+        collapseInlineTagWhitespace: true,
+      },
+      scriptLoading: 'defer',
+      publicPath: '',
+    }),
+  ],
+
+  module: {
+    rules: [
+      { test: /\.js$/, use: ['babel-loader'] },
+
+      { test: /\.(?:ico|gif|png|jpg|jpeg)$/i, type: 'asset/resource' },
+
+      { test: /\.(woff(2)?|eot|ttf|otf|svg|)$/, type: 'asset/inline' },
+    ],
+  },
+
+  resolve: {
+    modules: [paths.src, 'node_modules'],
+    extensions: ['.js', '.jsx', '.json'],
+    alias: {
+      '@': paths.src,
+      assets: paths.public,
+    },
+  },
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+// Importation de tous les fichiers
+function importAll(r) {
+  r.keys().forEach(r);
+}
+
+importAll(require.context('@/js/', true, /\.*$/));
+importAll(require.context('@/style/', true, /\.*$/));
